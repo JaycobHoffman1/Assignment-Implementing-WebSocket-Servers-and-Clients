6cd37910e93cf68e7c6144ac2da7a9984dd9367e
JaycobHoffman1 <jaycobhoffman2004@gmail.com>
2025-02-19 17:47:25 -0800

    mini project first commit

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from web_socket_server import WebSocketServer, socketio, app
 from flask import render_template
 
 app = WebSocketServer().create_app()
-# Task 1: Refactor the messages list to use a Hashmap called message_storage where the key is the author and the value is a list of messages.
-message_storage = {}
+messages = []
 
 @socketio.on('connect')
 def handle_connect():
@@ -14,17 +13,14 @@ def handle_disconnect():
     print('Client disconnected')
 
 @socketio.on('message')
-# Task 2: Modify the handle_message function to store messages in the message_storage Hashmap.
-# Task 3: Update the socketio.on('message') event handler to retrieve messages from the message_storage HashMap.
 def handle_message(message):
     print(f'Received message: {message}')
-    message_storage['John Doe'] = message
+    messages.append(message)
     socketio.emit('message', message)
 
 @socketio.on('get_user_messages')
-# Task 5: Update socket get_all_messages to get all messages sending by a specific user.
 def handle_get_user_messages(data):
-    socketio.emit('get_user_messages', message_storage['John Doe'])
+    socketio.emit('get_user_messages', messages)
 
 @app.route('/')
 def index():
